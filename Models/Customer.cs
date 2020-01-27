f019288f5e16f606fe64dffca286da3ef99566f2
Patricia Schneider <ps206415@ohio.edu>
2020-01-27 10:35:46 -0500

    Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ps206415_MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        //if you type prop for property the tab tab it will give property
+        public string customerLastName { get; set; }
+        public string customerFirstName { get; set; }
+
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+    }
+}
