0738287d942cdc5be25082ab82fe8ba3b090281d
Patricia Schneider <ps206415@ohio.edu>
2020-02-09 15:53:40 -0500

    Added Student, Course, and Enrollment

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ps206415_MIS4200.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        //if you type prop for property the tab tab it will give property
+        public string studentLastName { get; set; }
+        public string studentFirstName { get; set; }
+
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime studentSince { get; set; }
+
+        // a student can be enrolled in several classes
+        public ICollection<Enrollment> Enrollment { get; set; }
+    }
+}
